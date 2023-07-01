552a5a0d5b5e1723d63faf9c5a062a47e2e354c3
Novke <novke.brate@gmail.com>
2023-07-01 12:54:02 +0200

    1

---
+++ Zajednicki/src/Konstate/Operacije.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Konstate;
+
+
+public interface Operacije {
+    
+    public static final int LOGIN = 0;
+    
+}
---
+++ Zajednicki/src/domen/Radnik.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domen;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Novica
+ */
+public class Radnik implements Serializable{
+    
+    private String ime;
+    private String prezime;
+    private String email;
+    private String lozinka;
+
+    public Radnik() {
+    }
+
+    public Radnik(String ime, String prezime, String email, String lozinka) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.lozinka = lozinka;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
